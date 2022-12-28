28edffd912146fc99d969bf1f2de90280c7b7cef
nogukou0124 <ee207078@meiji.ac.jp>
2022-12-28 12:10:23 +0900

    事前確認ファイルの修正

--- confirm.py
+++ confirm.py
@@ -2,10 +2,10 @@ import sys
 from kaggle_environments import make,utils,agent
 
 out = sys.stdout
-submission = utils.read_file("./submission.py")
-a = agent.get_last_callable(submission)
+submission = agent.read_file("./submission.py")
+a = agent.get_last_callable(submission, path="./")
 sys.stdout = out
 
 env = make("connectx", debug=True)
-env.run([agent, agent])
+env.run([a, a])
 print("Success" if env.state[0].status == env.state[1].status == "DONE" else "Failed")
